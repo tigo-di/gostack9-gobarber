bec0389a9e61512a9013cbefb9917a64e58ef097
Tiago Dias <tigo@webdeleve.net>
2019-10-20 02:44:21 -0300

    feat: Generating password with bcrypt. See Hook BeforeSave in model User.js

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
@@ -6,15 +7,34 @@ class User extends Model {
     // Initialize a model, representing a table in the DB, with attributes and options.
     super.init(
       {
+        /*
+          As colunas presentes no model
+          não precisam
+          ser um reflexo das colunas presentes
+          na tabela no banco de dados.
+
+          
+        */
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL, // um campo que nunca vai existir no bd, somente existe aqui no código
         password_hash: Sequelize.STRING,
         provider: Sequelize.BOOLEAN,
       },
       {
         sequelize,
       }
-    );
+    ); // fim de super.init
+
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        // se há = qnd user criado ou senha alterada
+
+        user.password_hash = await bcrypt.hash(user.password, 8); // 8 = número de rounds/passos da cryptografia
+      }
+    }); // Hook = função do Sequelize, trechos de código que são executados de acordo com ações que acontecem em nosso model.
+
+    // return this;
   }
 }
 
