6144a3a6903ec5ebdb33747167acf4b7eb13168a
Tiago Dias <tigo@webdeleve.net>
2019-10-20 15:42:52 -0300

    doc: more comments in UserController.js

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -3,15 +3,19 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    // verificar se usuário existe
+    // VERIFICA se existe usuário com o email informado
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
+    // INTERROMPE fluxo se já existe usuário com o email informado
     if (userExists) {
       return res.status(400).json({ error: 'User already exists.' });
     }
 
-    // vai recebber dados de reatc, insomina e criar um novo usuário
+    // RECEBE dados de reatc, insomia e
+    // CRIA com Sequelize um novo usuário
     const { id, name, email, provider } = await User.create(req.body);
+
+    // RETORNA
     return res.json({
       id,
       name,
