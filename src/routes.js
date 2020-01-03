700d8461d8f1aa7acafcb69cb3fca03735bb07b1
Tiago Dias <tigo@webdeleve.net>
2020-01-03 04:42:23 -0300

    feat: cancel appointment

--- src/routes.js
+++ src/routes.js
@@ -45,8 +45,11 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 routes.post('/files', upload.single('file'), FileController.store);
+
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+routes.delete('/appointments/:id', AppointmentController.delete);
+
 routes.get('/schedules', ScheduleController.index);
 routes.get('/notifications', NotificationController.index);
 routes.put('/notifications/:id', NotificationController.update);
