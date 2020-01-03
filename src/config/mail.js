03c2996e9c784a6ac7f10d989d6a61170d9b10a1
Tiago Dias <tigo@webdeleve.net>
2020-01-03 06:41:39 -0300

    feat: appointment cancellation email to the provider

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '8652a72e03da91',
+    pass: '659442a7f13031',
+  },
+  default: {
+    from: 'Team Gorber <noreply@gobarber.com>',
+  },
+};
